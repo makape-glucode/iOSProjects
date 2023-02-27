cecadcd0d097baf7f1c7d16f67ea2efb735e784d
makape-glucode <124408901+makape-glucode@users.noreply.github.com>
2023-02-27 09:44:18 +0200

    Added To-Do project

---
+++ toDoList/toDoList/toDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  toDoList
+//
+//  Created by Makape Tema on 2023/02/26.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ toDoList/toDoList/toDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  toDoList
+//
+//  Created by Makape Tema on 2023/02/26.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
